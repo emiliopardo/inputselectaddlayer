7b86a980cdc167329d91dd67974b80ae1b1c6329
Emilio Pardo <e.pardo.perez@gmail.com>
2021-09-07 12:07:32 +0200

    Se limpia código por defecto

--- src/facade/js/inputselectaddlayercontrol.js
+++ src/facade/js/inputselectaddlayercontrol.js
@@ -67,39 +67,6 @@ export default class InputselectaddlayerControl extends M.Control {
   }
 
   /**
-   * This function is called on the control activation
-   *
-   * @public
-   * @function
-   * @api stable
-   */
-  activate() {
-    // calls super to manage de/activation
-    super.activate();
-    const div = document.createElement('div');
-    div.id = 'msgInfo';
-    div.classList.add('info');
-    div.innerHTML = 'Haz doble click sobre el mapa';
-    this.map_.getContainer().appendChild(div);
-
-    this.getImpl().activateClick(this.map_);
-  }
-  /**
-   * This function is called on the control deactivation
-   *
-   * @public
-   * @function
-   * @api stable
-   */
-  deactivate() {
-    // calls super to manage de/activation
-    super.deactivate();
-    const div = document.getElementById('msgInfo');
-    this.map_.getContainer().removeChild(div);
-
-    this.getImpl().deactivateClick(this.map_);
-  }
-  /**
    * This function gets activation button
    *
    * @public
