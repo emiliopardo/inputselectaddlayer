603585d26a0639f15d7f2a71b2c9a42fa4c889b8
emiliopardo <e.pardo.perez@gmail.com>
2021-09-09 14:05:13 +0200

    añadidos nuevos ejemplos config

--- test/test.js
+++ test/test.js
@@ -188,10 +188,291 @@ const configTipologiasConstrucctivas = {
   ]
 }
 
+
+const configCaracterizacionEspacioConstruido = {
+  title: 'Caracterización y distribución del espacio construido en Andalucía. Viviendas',
+  group: true,
+  groupTypes: ['optiongroup'],
+  layerGroups: [
+    {
+      optiongroup: 'Número de viviendas',
+      layers: [
+        {
+          id: 1,
+          name: 'gridcatv_250',
+          title: 'Número total de viviendas',
+          style: 'stl_viviendas_nviv',
+          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
+        },
+        {
+          id: 2,
+          name: 'gridcatv_250',
+          title: 'Superficie de las viviendas (Mediana)',
+          style: 'stl_viviendas_m2viv_mediana',
+          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
+        },
+        {
+          id: 3,
+          name: 'gridcatv_250',
+          title: 'Superficie total de las viviendas',
+          style: 'stl_viviendas_m2viv_total',
+          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
+        }
+      ]
+    },
+    {
+      optiongroup: 'Antigüedad de la vivienda',
+      layers: [
+        {
+          id: 4,
+          name: 'gridcatv_250',
+          title: 'Año de antigüedad (mediana)',
+          style: 'stl_viviendas_m2viv_total',
+          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
+        }
+      ]
+    },
+    {
+      optiongroup: 'Tipologías constructivas',
+      layers: [
+        {
+          id: 5,
+          name: 'gridcatv_250',
+          title: 'Número de viviendas colectivas',
+          style: 'stl_viviendas_nvivpluri',
+          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
+        },
+        {
+          id: 6,
+          name: 'gridcatv_250',
+          title: 'Número de viviendas unifamiliares aisladas o pareadas',
+          style: 'stl_viviendas_nvivunif1',
+          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
+        },
+        {
+          id: 7,
+          name: 'gridcatv_250',
+          title: 'Número de viviendas unifamiliares en líneas o manzana cerrada',
+          style: 'stl_viviendas_nvivunif2',
+          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
+        }
+      ]
+    },
+    {
+      optiongroup: 'Viviendas con piscina',
+      layers: [
+        {
+          id: 8,
+          name: 'gridcatv_250',
+          title: 'Número de viviendas con piscina',
+          style: 'stl_viviendas_nviv_h2o',
+          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
+        },
+        {
+          id: 9,
+          name: 'gridcatv_250',
+          title: 'Número de viviendas con piscina de uso unifamiliar',
+          style: 'stl_viviendas_nvivh2ouni',
+          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
+        },
+        {
+          id: 10,
+          name: 'gridcatv_250',
+          title: 'Número de viviendas con piscina de uso comunitario',
+          style: 'stl_viviendas_nvivh2ocom',
+          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
+        }
+      ]
+    },
+    {
+      optiongroup: 'Viviendas con espacios abiertos',
+      layers: [
+        {
+          id: 11,
+          name: 'gridcatv_250',
+          title: 'Número de viviendas en parcelas con espacios abiertos (patio/jardín)',
+          style: 'stl_viviendas_nviv_ea',
+          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
+        },
+        {
+          id: 12,
+          name: 'gridcatv_250',
+          title: 'Número de viviendas unifamiliares en parcelas con espacio abierto (patio/jardí­n)',
+          style: 'stl_viviendas_nvivuni_ea',
+          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
+        },
+        {
+          id: 13,
+          name: 'gridcatv_250',
+          title: 'Número de viviendas plurifamiliares en parcelas con espacio abierto (patio/jardí­n)',
+          style: 'stl_viviendas_nvivplu_ea',
+          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
+        }
+      ]
+    },
+    {
+      optiongroup: 'Viviendas con garaje',
+      layers: [
+        {
+          id: 14,
+          name: 'gridcatv_250',
+          title: 'Número de viviendas en parcelas con garaje',
+          style: 'stl_viviendas_nviv_app',
+          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
+        },
+        {
+          id: 15,
+          name: 'gridcatv_250',
+          title: 'Número de viviendas unifamiliares en parcelas con garaje',
+          style: 'stl_viviendas_nvivuniapp',
+          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
+        },
+        {
+          id: 16,
+          name: 'gridcatv_250',
+          title: 'Número de viviendas plurifamiliares en parcelas con garaje',
+          style: 'stl_viviendas_nvivpluapp',
+          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
+        }
+      ]
+    }
+  ]
+}
+
 const configOrtofotografias = {
   title: 'Ortofotografías Digitales de Andalucía',
   group: true,
-  type: 'optiongroup',
+  groupTypes: ['optiongroup'],
+  layerGroups: [
+    {
+      optiongroup: 'Año 2016',
+      layers: [
+        {
+          id: 9,
+          name: 'ortofotografia_2016_pancromatico',
+          title: 'Ortofotografía Pancromática 0,5 metros/pixel (Año 2016)',
+          style: 'default',
+          url: 'http://www.ideandalucia.es/wms/ortofoto2016?'
+        },
+        {
+          id: 10,
+          name: 'ortofotografia_2016_rgb',
+          title: 'Ortofotografía Color 0,5 metros/pixel (Año 2016)',
+          style: 'default',
+          url: 'http://www.ideandalucia.es/wms/ortofoto2016?'
+        },
+        {
+          id: 11,
+          name: 'ortofotografia_2016_infrarrojo',
+          title: 'Ortofotografía Infrarrojo 0,5 metros/pixel (Año 2016)',
+          style: 'default',
+          url: 'http://www.ideandalucia.es/wms/ortofoto2016?'
+        }
+      ]
+    },
+    {
+      optiongroup: 'Año 2013',
+      layers: [
+        {
+          id: 8,
+          name: 'OCA05_2013',
+          title: 'Ortofotografía en Color 0,5 metros/pixel (Año 2013)',
+          style: 'default',
+          url: 'http://www.ideandalucia.es/wms/ortofoto2013?'
+        }
+      ]
+    },
+    {
+      optiongroup: 'Año 2011',
+      layers: [
+        {
+          id: 7,
+          name: 'OCA10_2010',
+          title: 'Ortofotografía en Color 0,5 metros/pixel (Año 2011)',
+          style: 'default',
+          url: 'http://www.ideandalucia.es/wms/ortofoto2010?'
+        }
+      ]
+    },
+    {
+      optiongroup: 'Año 2009',
+      layers: [
+        {
+          id: 6,
+          name: 'OCA10_2009',
+          title: 'Ortofotografía en Color 0,5 metros/pixel (Año 2009)',
+          style: 'default',
+          url: 'http://www.ideandalucia.es/wms/ortofoto2009?'
+        }
+      ]
+    },
+    {
+      optiongroup: 'Año 2007',
+      layers: [
+        {
+          id: 5,
+          name: 'ORTO_2007',
+          title: 'Ortofotografía en Color 1 metro/pixel (Año 2007)',
+          style: 'default',
+          url: 'http://www.ideandalucia.es/wms/ortofoto2007?'
+        }
+      ]
+    },
+    {
+      optiongroup: 'Año 2004',
+      layers: [
+        {
+          id: 4,
+          name: 'ORTO_2004',
+          title: 'Ortofotografía en Color 1 metro/pixel (Año 2004)',
+          style: 'default',
+          url: 'http://www.ideandalucia.es/wms/ortofoto2004?'
+        }
+      ]
+    },
+    {
+      optiongroup: 'Año 2001',
+      layers: [
+        {
+          id: 3,
+          name: 'ORTO_2001',
+          title: 'Ortofotografía Pancromática 0,5 metros/pixel  (Año 2001)',
+          style: 'default',
+          url: 'http://www.ideandalucia.es/wms/ortofoto2001?'
+        }
+      ]
+    },
+    {
+      optiongroup: 'Año 1998',
+      layers: [
+        {
+          id: 2,
+          name: 'ortofoto1998',
+          title: 'Ortofotografía en Color 1 metro/pixel (Año 1998)',
+          style: 'default',
+          url: 'http://www.ideandalucia.es/wms/ortofoto1998?'
+        }
+      ]
+    },
+    {
+      optiongroup: 'Año 1956',
+      layers: [
+        {
+          id: 1,
+          name: 'REDIAM',
+          title: 'Ortofotografía Pancromática 1 metro/pixel (Año 1956)',
+          style: 'default',
+          url: 'http://www.juntadeandalucia.es/medioambiente/mapwms/REDIAM_Ortofoto_Andalucia_1956?'
+        }
+      ]
+    },
+  ]
+}
+
+const configOrtofotografias2 = {
+  title: 'Ortofotografías Digitales de Andalucía',
+  group: true,
+  groupTypes: ['group'],
   layerGroups: [
     {
       group: 'Año 2016',
@@ -318,160 +599,151 @@ const configOrtofotografias = {
   ]
 }
 
-const configCaracterizacionEspacioConstruido = {
-  title: 'Caracterización y distribución del espacio construido en Andalucía. Viviendas',
+const configOrtofotografias3 = {
+  title: 'Ortofotografías Digitales de Andalucía',
   group: true,
-  type: 'optiongroup',
+  groupTypes: ['group','optiongroup'],
   layerGroups: [
     {
-      group: 'Número de viviendas',
+      group: 'Año 2016',
+      optionGroup: 'Año 2016',
       layers: [
-        {
-          id: 1,
-          name: 'gridcatv_250',
-          title: 'Número total de viviendas',
-          style: 'stl_viviendas_nviv',
-          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
-        },
-        {
-          id: 2,
-          name: 'gridcatv_250',
-          title: 'Superficie de las viviendas (Mediana)',
-          style: 'stl_viviendas_m2viv_mediana',
-          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
-        },
-        {
-          id: 3,
-          name: 'gridcatv_250',
-          title: 'Superficie total de las viviendas',
-          style: 'stl_viviendas_m2viv_total',
-          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
-        }
-      ]
-    },
-    {
-      group: 'Antigüedad de la vivienda',
-      layers: [
-        {
-          id: 4,
-          name: 'gridcatv_250',
-          title: 'Año de antigüedad (mediana)',
-          style: 'stl_viviendas_m2viv_total',
-          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
-        }
-      ]
-    },
-    {
-      group: 'Tipologías constructivas',
-      layers: [
-        {
-          id: 5,
-          name: 'gridcatv_250',
-          title: 'Número de viviendas colectivas',
-          style: 'stl_viviendas_nvivpluri',
-          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
-        },
-        {
-          id: 6,
-          name: 'gridcatv_250',
-          title: 'Número de viviendas unifamiliares aisladas o pareadas',
-          style: 'stl_viviendas_nvivunif1',
-          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
-        },
-        {
-          id: 7,
-          name: 'gridcatv_250',
-          title: 'Número de viviendas unifamiliares en líneas o manzana cerrada',
-          style: 'stl_viviendas_nvivunif2',
-          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
-        }
-      ]
-    },
-    {
-      group: 'Viviendas con piscina',
-      layers: [
-        {
-          id: 8,
-          name: 'gridcatv_250',
-          title: 'Número de viviendas con piscina',
-          style: 'stl_viviendas_nviv_h2o',
-          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
-        },
         {
           id: 9,
-          name: 'gridcatv_250',
-          title: 'Número de viviendas con piscina de uso unifamiliar',
-          style: 'stl_viviendas_nvivh2ouni',
-          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
+          name: 'ortofotografia_2016_pancromatico',
+          title: 'Ortofotografía Pancromática 0,5 metros/pixel (Año 2016)',
+          style: 'default',
+          url: 'http://www.ideandalucia.es/wms/ortofoto2016?'
         },
         {
           id: 10,
-          name: 'gridcatv_250',
-          title: 'Número de viviendas con piscina de uso comunitario',
-          style: 'stl_viviendas_nvivh2ocom',
-          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
-        }
-      ]
-    },
-    {
-      group: 'Viviendas con espacios abiertos',
-      layers: [
+          name: 'ortofotografia_2016_rgb',
+          title: 'Ortofotografía Color 0,5 metros/pixel (Año 2016)',
+          style: 'default',
+          url: 'http://www.ideandalucia.es/wms/ortofoto2016?'
+        },
         {
           id: 11,
-          name: 'gridcatv_250',
-          title: 'Número de viviendas en parcelas con espacios abiertos (patio/jardín)',
-          style: 'stl_viviendas_nviv_ea',
-          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
-        },
-        {
-          id: 12,
-          name: 'gridcatv_250',
-          title: 'Número de viviendas unifamiliares en parcelas con espacio abierto (patio/jardí­n)',
-          style: 'stl_viviendas_nvivuni_ea',
-          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
-        },
-        {
-          id: 13,
-          name: 'gridcatv_250',
-          title: 'Número de viviendas plurifamiliares en parcelas con espacio abierto (patio/jardí­n)',
-          style: 'stl_viviendas_nvivplu_ea',
-          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
+          name: 'ortofotografia_2016_infrarrojo',
+          title: 'Ortofotografía Infrarrojo 0,5 metros/pixel (Año 2016)',
+          style: 'default',
+          url: 'http://www.ideandalucia.es/wms/ortofoto2016?'
         }
       ]
     },
     {
-      group: 'Viviendas con garaje',
+      group: 'Año 2013',
+      optionGroup: 'Año 2013',
       layers: [
         {
-          id: 14,
-          name: 'gridcatv_250',
-          title: 'Número de viviendas en parcelas con garaje',
-          style: 'stl_viviendas_nviv_app',
-          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
-        },
-        {
-          id: 15,
-          name: 'gridcatv_250',
-          title: 'Número de viviendas unifamiliares en parcelas con garaje',
-          style: 'stl_viviendas_nvivuniapp',
-          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
-        },
-        {
-          id: 16,
-          name: 'gridcatv_250',
-          title: 'Número de viviendas plurifamiliares en parcelas con garaje',
-          style: 'stl_viviendas_nvivpluapp',
-          url: 'https://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/gridcatv/wms?',
+          id: 8,
+          name: 'OCA05_2013',
+          title: 'Ortofotografía en Color 0,5 metros/pixel (Año 2013)',
+          style: 'default',
+          url: 'http://www.ideandalucia.es/wms/ortofoto2013?'
         }
       ]
-    }
+    },
+    {
+      group: 'Año 2011',
+      optionGroup: 'Año 2011',
+      layers: [
+        {
+          id: 7,
+          name: 'OCA10_2010',
+          title: 'Ortofotografía en Color 0,5 metros/pixel (Año 2011)',
+          style: 'default',
+          url: 'http://www.ideandalucia.es/wms/ortofoto2010?'
+        }
+      ]
+    },
+    {
+      group: 'Año 2009',
+      optionGroup: 'Año 2009',
+      layers: [
+        {
+          id: 6,
+          name: 'OCA10_2009',
+          title: 'Ortofotografía en Color 0,5 metros/pixel (Año 2009)',
+          style: 'default',
+          url: 'http://www.ideandalucia.es/wms/ortofoto2009?'
+        }
+      ]
+    },
+    {
+      group: 'Año 2007',
+      optionGroup: 'Año 2007',
+      layers: [
+        {
+          id: 5,
+          name: 'ORTO_2007',
+          title: 'Ortofotografía en Color 1 metro/pixel (Año 2007)',
+          style: 'default',
+          url: 'http://www.ideandalucia.es/wms/ortofoto2007?'
+        }
+      ]
+    },
+    {
+      group: 'Año 2004',
+      optionGroup: 'Año 2004',
+      layers: [
+        {
+          id: 4,
+          name: 'ORTO_2004',
+          title: 'Ortofotografía en Color 1 metro/pixel (Año 2004)',
+          style: 'default',
+          url: 'http://www.ideandalucia.es/wms/ortofoto2004?'
+        }
+      ]
+    },
+    {
+      group: 'Año 2001',
+      optionGroup: 'Año 2001',
+      layers: [
+        {
+          id: 3,
+          name: 'ORTO_2001',
+          title: 'Ortofotografía Pancromática 0,5 metros/pixel  (Año 2001)',
+          style: 'default',
+          url: 'http://www.ideandalucia.es/wms/ortofoto2001?'
+        }
+      ]
+    },
+    {
+      group: 'Año 1998',
+      optionGroup: 'Año 1998',
+      layers: [
+        {
+          id: 2,
+          name: 'ortofoto1998',
+          title: 'Ortofotografía en Color 1 metro/pixel (Año 1998)',
+          style: 'default',
+          url: 'http://www.ideandalucia.es/wms/ortofoto1998?'
+        }
+      ]
+    },
+    {
+      group: 'Año 1956',
+      optionGroup: 'Año 1956',
+      layers: [
+        {
+          id: 1,
+          name: 'REDIAM',
+          title: 'Ortofotografía Pancromática 1 metro/pixel (Año 1956)',
+          style: 'default',
+          url: 'http://www.juntadeandalucia.es/medioambiente/mapwms/REDIAM_Ortofoto_Andalucia_1956?'
+        }
+      ]
+    },
   ]
 }
 
-const mp = new Inputselectaddlayer(configOrtofotografias);
+//const mp = new Inputselectaddlayer(configOrtofotografias);
+//const mp = new Inputselectaddlayer(configOrtofotografias2);
+const mp = new Inputselectaddlayer(configOrtofotografias3);
 //const mp = new Inputselectaddlayer(configDirectorioEmpresas);
 //const mp = new Inputselectaddlayer(configTipologiasConstrucctivas);
-// const mp = new Inputselectaddlayer(configCaracterizacionEspacioConstruido);
+//const mp = new Inputselectaddlayer(configCaracterizacionEspacioConstruido);
 
 
 map.addPlugin(mp);
