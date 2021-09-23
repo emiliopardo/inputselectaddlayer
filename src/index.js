71d8dc181b74f0c787c1f187e3d62c0bdabc1ff6
emiliopardo <e.pardo.perez@gmail.com>
2021-09-23 09:02:56 +0200

    Se genera build

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import M$plugin$Inputselectaddlayer from '/home/epardo/proyectos/inputselectaddlayer/src/facade/js/inputselectaddlayer';
+import M$control$InputselectaddlayerControl from '/home/epardo/proyectos/inputselectaddlayer/src/facade/js/inputselectaddlayercontrol';
+import M$impl$control$InputselectaddlayerControl from '/home/epardo/proyectos/inputselectaddlayer/src/impl/ol/js/inputselectaddlayercontrol';
+
+if (!window.M.plugin) window.M.plugin = {};
+if (!window.M.control) window.M.control = {};
+if (!window.M.impl) window.M.impl = {};
+if (!window.M.impl.control) window.M.impl.control = {};
+window.M.plugin.Inputselectaddlayer = M$plugin$Inputselectaddlayer;
+window.M.control.InputselectaddlayerControl = M$control$InputselectaddlayerControl;
+window.M.impl.control.InputselectaddlayerControl = M$impl$control$InputselectaddlayerControl;
